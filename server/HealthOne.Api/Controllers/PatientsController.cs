80dad1938bd3d4b60f3cdcf45a5cd4c174516886
kiran23 <94864444+kiransaravade@users.noreply.github.com>
2026-06-15 15:17:44 +0530

    Update PatientsController.cs

--- server/HealthOne.Api/Controllers/PatientsController.cs
+++ server/HealthOne.Api/Controllers/PatientsController.cs
@@ -12,7 +12,7 @@ public class PatientsController : ControllerBase
         {
             new() { Id = 1, Name = "Rick Smith", Age = 35, Gender = "Male" },
             new() { Id = 2, Name = "Mary Jones", Age = 29, Gender = "Female" },
-            new() { Id = 3, Name = "David Brown", Age = 42, Gender = "Male" },
+            new() { Id = 3, Name = "Jockey Brown", Age = 42, Gender = "Male" },
             new() { Id = 4, Name = "Kiran Saravade", Age = 21, Gender = "Male" },
             new() { Id = 4, Name = "Virat", Age = 21, Gender = "Male" }
         };
